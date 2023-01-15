a9176e5979e4ebcc3fcc419ab94b273a2e741a89
sisirdhakal <dhakalsaurav023@gmail.com>
2023-01-15 11:26:30 +0545

    new package added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,34 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import store from '../states/store'
+import Footer from '../components/footer'
+import { useRouter } from 'next/router'
+import NextJSProgress from 'nextjs-progressbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return <>
+    <Provider store={store}>
+      <div key={router.pathname} className='h-screen grid grid-rows-nav'>
+        <NextJSProgress
+          color="red"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+        <div className='grid grid-rows-auto relative'>
+          {getLayout(<Component {...pageProps} />)}
+          {/* <div className='bottom-0 absolute w-full'>
+            <Footer />
+          </div> */}
+        </div>
+      </div>
+    </Provider>
+  </>
 }
+
+export default MyApp
