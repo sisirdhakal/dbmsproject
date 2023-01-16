2d9d722883a67c0a47a12cf1b00a9c4ca795fbc8
sisirdhakal <dhakalsaurav023@gmail.com>
2023-01-16 23:10:50 +0545

    mysql database

---
+++ backend/db/index.ts
@@ -0,0 +1,13 @@
+var mysql = require('mysql2');
+
+// create the connection to database
+const db1 = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1044code',
+    database: 'mydb',
+});
+
+
+
+module.exports = db1;
