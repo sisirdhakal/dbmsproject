7203d53cc743cba452bb666c532225c88af0a603
sisirdhakal <dhakalsaurav023@gmail.com>
2023-01-17 22:16:56 +0545

    task controller

--- backend/controllers/taskController.ts
+++ backend/controllers/taskController.ts
@@ -1,8 +1,66 @@
 
 
-const getAllTask = async () => {
+const getAllTasks = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
 
 }
+
+const createTask = async (req, res, next) => {
+
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const getSingleTask = async (req, res, next) => {
+
+    try {
+
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateTask = async (req, res, next) => {
+
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const updateStatus = async (req, res, next) => {
+
+    try {
+      
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const deleteTask = async (req, res, next) => {
+
+    try {
+
+
+    } catch (error) {
+
+    }
+}
+
+
 
 module.exports = {
     getAllTask
